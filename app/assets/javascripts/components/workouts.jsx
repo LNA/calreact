2556fe091d36ef8bbe10a128acab2e95c34c3474
LNA <LaToyaNAllen@gmail.com>
2017-03-25 14:44:37 +0930

    Replace partial with component

--- app/assets/javascripts/components/workouts.jsx
+++ app/assets/javascripts/components/workouts.jsx
@@ -1,7 +1,17 @@
 var Workouts = React.createClass({
   render: function() {
     return (
-        <h1>CalReact</h1>
-        )
+      <div>
+       {this.props.workouts.map(function(workout) {
+          return (
+           <div>
+             <h3>{workout.name}</h3>
+             <p>{workout.apt_time}</p>
+           </div>
+
+          )
+        })}
+       </div>
+      )
   }
 });
